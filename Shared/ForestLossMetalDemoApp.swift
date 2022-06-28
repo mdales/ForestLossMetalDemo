b378c072c40b841f8f19d06704c470fb81eb3493
Michael Dales <michael@digitalflapjack.com>
2022-06-28 10:05:10 +0100

    Initial Commit

---
+++ Shared/ForestLossMetalDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForestLossMetalDemoApp.swift
+//  Shared
+//
+//  Created by Michael Dales on 28/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ForestLossMetalDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
